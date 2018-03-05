14c2f2d3b596d89c74e9ea0c4bc35c0b3f661459
ingin97 <ingi.n97@gmail.com>
2018-03-05 12:08:13 +0100

    Detect line/avstand
    Laget kode for deteksjon av linje og ting i nærheten

--- avstand/avstand.ino
+++ avstand/avstand.ino
@@ -1,25 +1,16 @@
-int echoPin = 6;
-int triggerPin=7;
-
-
 int echoPin = 2;
-int triggerPin=3;
+int triggerPin = 3;
 
 void setup() {
   Serial.begin(9600);
-  
   pinMode(triggerPin,OUTPUT);
   pinMode(echoPin, INPUT);
-  
-
 }
 
 void loop() {
 
   Serial.println("Avstand "+String(sjekk_avstand()));
-  //Serial.println("Avstand1 "+String(sjekk_avstand()));
-  
-
+  delay(500);
 }
 
 double sjekk_avstand(){
@@ -33,7 +24,6 @@ double sjekk_avstand(){
 
   long duration = pulseIn(echoPin, 1);
   float distance = duration * 0.00017;
-  //Serial.println(distance);
   
-  return (distance1, distance2);
+  return (distance);
 }
